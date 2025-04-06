237c3eb60b63b31ebc9427f776f0904fe9e4031d
GnatNamedAsh <33440472+GnatNamedAsh@users.noreply.github.com>
2025-04-06 03:03:26 -0500

    codecrafters submit [skip ci]

--- src/kafka/server.rs
+++ src/kafka/server.rs
@@ -13,6 +13,12 @@ pub struct Connection {
     api_version: u16,
 }
 
+struct ApiVersion {
+    api_key: i16,
+    min_version: u16,
+    max_version: u16,
+}
+
 impl Connection {
     pub fn new(stream: TcpStream) -> Self {
         Connection {
@@ -71,26 +77,38 @@ impl Connection {
             response_body[0..2].copy_from_slice(&35_i16.to_be_bytes());
             return Ok((response_body, 2));
         }
+        println!("Getting supported apis");
+        let supported_apis = Self::get_supported_apis();
 
         // There needs to be a tag buffer between keys, and a tag buffer after the throttle_time_ms
         // error code
         response_body[0..2].copy_from_slice(&0_i16.to_be_bytes());
         // num_api_keys
-        response_body[2..3].copy_from_slice(&2_i8.to_be_bytes());
+        let num_api_keys = supported_apis.len() as i8;
+        // kafka takes the length of the array + 1 as the number of api keys
+        response_body[2..3].copy_from_slice(&(num_api_keys + 1).to_be_bytes());
         // api_keys ->
-        // api_key
-        response_body[3..5].copy_from_slice(&18_i16.to_be_bytes());
-        // min_version
-        response_body[5..7].copy_from_slice(&0_i16.to_be_bytes());
-        // max_version
-        response_body[7..9].copy_from_slice(&4_i16.to_be_bytes());
-        // tag buffer
-        response_body[9..10].copy_from_slice(tag_buffer);
+        let mut start_index = 3;
+        for api in supported_apis {
+            response_body[start_index..start_index + 2].copy_from_slice(&api.api_key.to_be_bytes());
+            start_index += 2;
+            response_body[start_index..start_index + 2]
+                .copy_from_slice(&api.min_version.to_be_bytes());
+            start_index += 2;
+            response_body[start_index..start_index + 2]
+                .copy_from_slice(&api.max_version.to_be_bytes());
+            start_index += 2;
+            response_body[start_index..start_index + 1].copy_from_slice(tag_buffer);
+            start_index += 1;
+        }
+
         // throttle_time_ms
-        response_body[10..14].copy_from_slice(&0_i32.to_be_bytes());
+        response_body[start_index..start_index + 4].copy_from_slice(&0_i32.to_be_bytes());
+        start_index += 4;
         // tag buffer
-        response_body[14..15].copy_from_slice(tag_buffer);
-        Ok((response_body, 15))
+        response_body[start_index..start_index + 1].copy_from_slice(tag_buffer);
+        println!("Response body going out");
+        Ok((response_body, (start_index + 1) as i32))
     }
 
     pub async fn write(&mut self) -> Result<(), io::Error> {
@@ -98,13 +116,35 @@ impl Connection {
         // uses an int16
         let mut header: [u8; 8] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
         header[4..8].copy_from_slice(&self.correlation_id.to_be_bytes());
-        let (response_body, body_size) = self.determine_api_key_func().unwrap()(self)?;
+        let (response_body, body_size) = match self.determine_api_key_func() {
+            Some(func) => func(self)?,
+            None => return Err(io::Error::new(io::ErrorKind::Other, "No function found")),
+        };
         header[0..4].copy_from_slice(&(body_size + 4).to_be_bytes());
+        println!("Header going out");
         self.stream.write(&header).await?;
+        println!("Body going out");
         let (body, _) = response_body.split_at(body_size as usize);
         self.stream.write(body).await?;
+        println!("Flushing");
         self.stream.flush().await?;
+        println!("Done");
         Ok(())
+    }
+
+    fn get_supported_apis() -> Vec<ApiVersion> {
+        vec![
+            ApiVersion {
+                api_key: 18,
+                min_version: 0,
+                max_version: 4,
+            },
+            ApiVersion {
+                api_key: 75,
+                min_version: 0,
+                max_version: 0,
+            },
+        ]
     }
 }
 
@@ -122,12 +162,12 @@ impl Server {
     pub async fn run(&self, addr: &str, port: u16) {
         let addr = format!("{}:{}", addr, port);
         let listener = TcpListener::bind(addr).await.unwrap();
-        println!("Server listening on {}", listener.local_addr().unwrap());
+        // println!("Server listening on {}", listener.local_addr().unwrap());
 
         loop {
             match listener.accept().await {
                 Ok((stream, peer_addr)) => {
-                    println!("Connection established: {}", peer_addr);
+                    // println!("Connection established: {}", peer_addr);
 
                     // Clone the Arc to the connections for this task
                     let connections = Arc::clone(&self.connections);
@@ -157,33 +197,31 @@ impl Server {
         connections: Arc<Mutex<HashMap<SocketAddr, Connection>>>,
         peer_addr: SocketAddr,
     ) {
+        // Get a clone of the connection to work with outside the mutex
+        let mut conn = {
+            let mut connections_lock = connections.lock().await;
+            if let Some(conn) = connections_lock.remove(&peer_addr) {
+                conn
+            } else {
+                println!("Connection not found: {}", peer_addr);
+                return;
+            }
+        };
+
         loop {
-            // Scope for the lock
-            {
-                let mut connections_lock = connections.lock().await;
-                if let Some(conn) = connections_lock.get_mut(&peer_addr) {
-                    match conn.read().await {
-                        Ok(message) => message,
-                        Err(e) => {
-                            println!("Error reading from connection {}: {}", peer_addr, e);
-                            break;
-                        }
-                    }
-                } else {
-                    println!("Connection not found: {}", peer_addr);
+            // Read operation
+            match conn.read().await {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error reading from connection {}: {}", peer_addr, e);
                     break;
                 }
             };
 
-            // Another scope for writing
-            {
-                let mut connections_lock = connections.lock().await;
-                if let Some(conn) = connections_lock.get_mut(&peer_addr) {
-                    if let Err(e) = conn.write().await {
-                        println!("Error writing to connection {}: {}", peer_addr, e);
-                        break;
-                    }
-                }
+            // Write operation
+            if let Err(e) = conn.write().await {
+                println!("Error writing to connection {}: {}", peer_addr, e);
+                break;
             }
         }
 
